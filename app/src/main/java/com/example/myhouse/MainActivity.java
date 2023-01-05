c46489f969d7c0c00a95292b37f0107cbb4ac2d5
Mohamed Ayman Ouchker <50711522+MohamedAymanOuchker@users.noreply.github.com>
2023-01-05 10:10:18 +0100

    Change design and add button functionality

--- app/src/main/java/com/example/myhouse/MainActivity.java
+++ app/src/main/java/com/example/myhouse/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.myhouse;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void Signup_su(View view) {
+        Intent t = new Intent(this,MainActivity4.class);
+        startActivity(t);
+    }
+
+    public void Login_su(View view) {
+        Intent t = new Intent(this,MainActivity2.class);
+        startActivity(t);
     }
 }
